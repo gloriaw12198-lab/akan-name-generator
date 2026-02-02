106acfd4278a01d2d1d7597dd0b32d2c20d1d15c
Gloria <gloriaw12198@gmail.com>
2026-02-02 19:17:47 +0300

    Added some detail to script.jss

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-function generateName() {
-    const birthdate = new Date(document.getElementById())
+
+function generateAkanName() {
+    // Get input values
+    const dob = document.getElementById("Date of birth").value;
+    const gender = document.getElementById("Gender").value;
+
+    // Parse date of birth
+    const birthDate = new Date(dob);
+    const day = birthDate.getDay(); // 0 = Sunday, 1 = Monday, ...
+
+    // Akan names based on day of the week
+    const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    // Determine Akan name
+    let akanName;
+    if (gender === "male") {
+        akanName = maleNames[day];
+    } else if (gender === "female") {
+        akanName = femaleNames[day];
+    } else {
+        akanName = "Please select a gender.";
+    }
+
+    // Display the result
+    document.getElementById("akan name").innerText = `Your Akan name is: ${akanName}`;
 }
+return 
+
